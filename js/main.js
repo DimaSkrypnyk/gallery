c50aaf872aaa23f196d61d3ff37632fb5fbd3f4a
DimaSkrypnyk <dmitro.skripnik@gmail.com>
2014-11-01 18:25:42 +0200

    some changes

--- js/main.js
+++ js/main.js
@@ -1,59 +1,87 @@
-$(document).ready(function(){
-    var container = $(".container");
-    var editBlock = $(".edit-block");
-    var gallery = $(".gallery");
-    var btn = editBlock.find('button');
-    var input = editBlock.find('input');
-    var tag, galleryDiv, imgDiv, currentGallery;
+var s,
+    FlickrGallery = {
 
-    input.keyup(function(){
-        $(".error").hide()
-    });
+        settings: {
+            imagesPerGallery: 4,
+            gallery: $('.gallery'),
+            addButton: $('#add-button'),
+            container: $('.container'),
+            input: $('.edit-block').find('input'),
+            list: $('.list')
+        },
 
-    editBlock.find('a').click(function(){
-        gallery.remove();
-    });
+        init: function() {
+            s = this.settings;
+            this.bindUIActions();
+        },
 
-
-    function addGallery(tag){
-        if(!tag){
-            $(".error").show();
-            return false
-        }
-
-        if(tag.indexOf(' ') >= 0)
-            tag = tag.replace(/\s/g, '');
-
-        $('.windows8').show();
-        container.hide();
-
-        imgDiv = '';
-        galleryDiv = '<div class="gallery" data-tag="' + tag + '"><h3>tag: ' + tag + '</h3></div>';
-        $(container).append(galleryDiv);
-        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
-                tags: tag,
-                format: "json"
-            },
-
-        function (data) {
-            console.log(data);
-            if(data.items.length == 0){
-                $('.gallery[data-tag=' + tag + ']').append('<h3>There are no such images with this tag</h3>');
-
-            }
-            $.each(data.items, function (i, item) {
-                if (i == 4) return false;
-                imgDiv += '<a href=' + item.link + ' target="_blank"><img data-tag="' + tag + ' "src="' + item.media.m + '" /></a>';
+        bindUIActions: function() {
+            s.input.keyup(function(){
+                $('.error').hide()
             });
 
-            $('.gallery[data-tag=' + tag + ']').append(imgDiv);
-            $('.windows8').hide();
-            container.show();
+            $('.remove-all').on('click', function(){
+                $('.gallery').add('.list li').remove();
+            });
 
-        });
-    }
+            $('body').on('click', '.remove-one',  function(){
+                FlickrGallery.removeGallery($(this).closest('li').attr('data-tag'));
+            });
 
-    btn.on('click', function(){
-        addGallery(input.val())
-    });
+            s.addButton.on('click', function() {
+                FlickrGallery.addGallery(s.imagesPerGallery, s.input.val());
+            });
+        },
+
+        addGallery: function(imagesPerGallery, tag) {
+            var imgDiv;
+            s.input.val('');
+
+            if(!tag){
+                $('.error').show();
+                return false
+            }
+
+            if(tag.indexOf(' ') >= 0)
+                tag = tag.replace(/\s/g, '');
+
+            $('.loader').show();
+            s.container.hide();
+
+            s.container.append('<div class="gallery" data-tag="' + tag + '"><h3>tag: ' + tag + '</h3></div>');
+            s.list.append('<li data-tag="' + tag + '">' + tag + '&nbsp;&nbsp;<a href="#" class="remove-one">Remove</a></li>');
+
+            imgDiv = '';
+            $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
+                    tags: tag,
+                    format: "json"
+                },
+
+                function (data) {
+                    console.log(data);
+                    if(data.items.length == 0)
+                        $('.gallery[data-tag=' + tag + ']').append('<h3>There are no such images with this tag</h3>');
+
+                    $.each(data.items, function (i, item) {
+                        if (i == s.imagesPerGallery) return false;
+                        imgDiv += '<a href=' + item.link + ' target="_blank"><img data-tag="' + tag + ' "src="' + item.media.m + '" /></a>';
+                    });
+
+                    $('.gallery[data-tag=' + tag + ']').append(imgDiv);
+                    $('.loader').hide();
+                    s.container.show();
+
+                });
+
+        },
+        removeGallery: function(tag) {
+            $('.gallery[data-tag=' + tag + ']').add('.list li[data-tag=' + tag + ']').remove()
+        }
+    };
+
+
+$(function(){
+    FlickrGallery.init();
 });
+
+
